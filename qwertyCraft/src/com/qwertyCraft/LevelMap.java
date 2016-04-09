72b0654f9b6910201df91706c8a73e8e804dc25a
Jeff <4jeffclark@gmail.com>
2016-04-09 10:16:48 -0700

    removing popJava submodule

--- qwertyCraft/src/com/qwertyCraft/LevelMap.java
+++ qwertyCraft/src/com/qwertyCraft/LevelMap.java
@@ -1,11 +1,25 @@
 package com.qwertyCraft;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class LevelMap {	
 	public Tile[][][] tile;
 	
-	public LevelMap () {
+	public LevelMap () throws IOException {
+
 		tile = new Tile[256][][];
-		
+        Yaml map = new Yaml();  
+        try( InputStream in = Files.newInputStream( Paths.get( "data\template.yml" ) ) ) {
+            Object qC_config = map.load( in );
+
+  					System.out.println( "dump:\n" + qC_config.toString() );
+        }
+
 		for (Integer x=0;x<256;x++) {
 			tile[x]= new Tile[256][];
 			for (Integer y=0;y<256;y++) {
